9f52f8830d3d1cee0a9cc5e094be73d9fd494e3a
syz10086 <s888666ok@163.com>
2025-01-12 22:55:45 +0800

    fix:update

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ module.exports = {
         return [
             {
                 source: '/terms', // 用户访问的路径
-                destination: '/en/TermsOfService', // 要重定向到的路径
+                destination: '/terms', // 要重定向到的路径
                 permanent: true, // 是否为永久重定向 (301)
             },
             {
                 source: '/privacy', // 用户访问的路径
-                destination: '/en/PrivacyPolicy', // 要重定向到的路径
+                destination: '/privacy', // 要重定向到的路径
                 permanent: true, // 是否为永久重定向 (301)
             },
         ];
