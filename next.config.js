2ad018147ae8fd97f39206f25b4cae22f55a7ef4
syz10086 <s888666ok@163.com>
2025-01-17 16:52:31 +0800

    fix:update

--- next.config.js
+++ next.config.js
@@ -4,12 +4,10 @@ module.exports = {
             {
                 source: '/terms', // 用户访问的路径
                 destination: '/terms', // 要重定向到的路径
-                permanent: true, // 是否为永久重定向 (301)
             },
             {
                 source: '/privacy', // 用户访问的路径
                 destination: '/privacy', // 要重定向到的路径
-                permanent: true, // 是否为永久重定向 (301)
             },
         ];
     },
